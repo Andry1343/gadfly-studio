0d85a065deab6d27057ff585274385cca2fbc2b3
Andry1343 <105875701+Andry1343@users.noreply.github.com>
2023-10-04 22:38:42 +0000

    Deploying to gh-pages from @ Andry1343/gadfly-studio@8fb8b1073e6777b89a269b2a42bf513c004edd8d 🚀

--- static/js/411.82c6806d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgadfly=self.webpackChunkgadfly||[]).push([[411],{411:function(e,n,t){t.r(n),t.d(n,{Home:function(){return l}});var i=t(184),l=function(){return(0,i.jsx)("div",{style:{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontSize:20,color:"#010101"},children:(0,i.jsx)("h1",{children:"Home Page"})})};n.default=l}}]);
-//# sourceMappingURL=411.82c6806d.chunk.js.map
